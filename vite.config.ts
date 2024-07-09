bcbb2926c487fbf4d1dc30723be3ca1defc373a8
Sofiya <zxcbogdanova@gmail.com>
2024-07-09 07:03:18 +0300

    test deploy fix via changing vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Укажите базовый путь, если ваше приложение размещено в подпапке
+  server: {
+    host: '0.0.0.0', // Разрешить подключения с любого IP-адреса
+    strictPort: true, // Автоматически выбрать другой порт, если указанный занят
+    open: true, // Автоматически открывать браузер после запуска
+  },
+  build: {
+    outDir: 'dist', // Папка для вывода сборки
+    assetsDir: '', // Не добавлять префикс к именам файлов
+  },
+});
