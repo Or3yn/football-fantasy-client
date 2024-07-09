9ba10793efa7349f15d50e6f9eb3e4fd559b0e0f
Sofiya <zxcbogdanova@gmail.com>
2024-07-09 09:57:33 +0300

    env var added

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Укажите базовый путь, если ваше приложение размещено в подпапке
-  server: {
-    host: '0.0.0.0', // Разрешить подключения с любого IP-адреса
-    strictPort: true, // Автоматически выбрать другой порт, если указанный занят
-    open: true, // Автоматически открывать браузер после запуска
-  },
-  build: {
-    outDir: 'dist', // Папка для вывода сборки
-    assetsDir: '', // Не добавлять префикс к именам файлов
-  },
+export default defineConfig(({ mode }) => {
+  const envDir = path.resolve(__dirname, "environment");
+  const env = loadEnv(mode, envDir, '');
+
+  return {
+    plugins: [react()],
+    base: './',
+    server: {
+      host: '0.0.0.0',
+      strictPort: true,
+      open: false,
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: '',
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+      __VITE_API_URL__: JSON.stringify(env.VITE_API_URL),
+      __VITE_API_KEY__: JSON.stringify(env.VITE_API_KEY),
+    },
+  };
 });
